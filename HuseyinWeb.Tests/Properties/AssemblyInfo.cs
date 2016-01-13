7b1697df9ab9546684527dbaac15e41737177b01
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2016-01-13 14:34:24 -0700

    git commit -m "commit new changes huseyin solution"

---
+++ HuseyinWeb.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HuseyinWeb.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Trimble Navigation Limited")]
+[assembly: AssemblyProduct("HuseyinWeb.Tests")]
+[assembly: AssemblyCopyright("Copyright © Trimble Navigation Limited 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("022d9490-346e-401b-abe7-ae0c7a94bf88")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HuseyinWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,50 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace HuseyinWeb
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/jquery.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/bootstrap.min.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
+               "~/Scripts/angular.js",
+               "~/Scripts/angular.min.js",
+               "~/Scripts/apps.js",
+               "~/Scripts/Controllers/MainController.js"
+               
+               ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.min.css",
+                "~/Content/bootstrap.css",
+                "~/Content/freelancer.css"));
+
+
+
+            //bundles.Add(new StyleBundle("~/Content/mvcgrid").Include(
+            //   "~/Content/Gridmvc.css"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/mvcgrid").Include(
+            //    "~/Scripts/gridmvc.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+        }
+    }
+}
